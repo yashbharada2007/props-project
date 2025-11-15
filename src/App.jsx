fde4f4b9324d5d3317d3abae1c610ae984dbab6f
Yash Bharada <yashbharada02@gmail.com>
2025-11-15 16:26:19 +0530

    intial commite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Card from "./components/Card";
+import User from "./components/User";
 
 const App = () => {
+  const arr = [10, 20, 30, 40, 50];
+
   return (
     <div className="parent">
-      <Card />
+      {arr.map(function (elem) {
+        return elem;
+      })}
     </div>
   );
 };
---
+++ src/components/User.jsx
@@ -0,0 +1,9 @@
+
+
+const User = (props) => {
+  return (
+    <div style={{color:'white'}}>{props.name}</div>
+  )
+}
+
+export default User
